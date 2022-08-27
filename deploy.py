fc116c6bab7aa7a94a027949c37805c843a86021
Jim Kaz <jimkaz60@gmail.com>
2022-08-27 15:45:02 +0000

    version2

--- deploy.py
+++ deploy.py
@@ -1,7 +1,8 @@
 # deploying a python web application
 echo "hello world" shell scrpts
 # shell scripts
-echo python "hello world" python scrpts
+#echo python "hello world" python scrpts
 # Pythin script
+print ('hello world')
 
 
